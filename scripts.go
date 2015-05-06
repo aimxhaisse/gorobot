595e642714dd633ff407eea815015b6ccf652481
s. rannou <mxs@sbrk.org>
2015-05-06 12:05:23 +0200

    Propagating environment to commands.

--- scripts.go
+++ scripts.go
@@ -63,6 +63,8 @@ func execCmd(config ScriptsConfig, path string, ev Event) {
 		ev.Channel,
 		ev.User)
 
+	command.Env = os.Environ()
+
 	for _, v := range in_params[1:] {
 		command.Args = append(command.Args, v)
 	}
